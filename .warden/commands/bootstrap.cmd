bb9bf8710bfa4d2b72280c8a50127e8aa64d088d
Jerry Lopez <me@jerrylopez.dev>
2022-11-27 02:47:11 +0000

    Sets up codespace proof of concept

---
+++ .warden/commands/bootstrap.cmd
@@ -0,0 +1,181 @@
+#!/usr/bin/env bash
+[[ ! ${WARDEN_DIR} ]] && >&2 echo -e "\033[31mThis script is not intended to be run directly!\033[0m" && exit 1
+set -euo pipefail
+
+function :: {
+  echo
+  echo "==> [$(date +%H:%M:%S)] $@"
+}
+
+## load configuration needed for setup
+WARDEN_ENV_PATH="$(locateEnvPath)" || exit $?
+loadEnvConfig "${WARDEN_ENV_PATH}" || exit $?
+assertDockerRunning
+
+## change into the project directory
+cd "${WARDEN_ENV_PATH}"
+
+## configure command defaults
+WARDEN_WEB_ROOT="$(echo "${WARDEN_WEB_ROOT:-/}" | sed 's#^/#./#')"
+REQUIRED_FILES=("${WARDEN_WEB_ROOT}/auth.json")
+DB_DUMP="${DB_DUMP:-./backfill/magento-db.sql.gz}"
+DB_IMPORT=1
+AUTO_PULL=1
+
+if [[ $CODESPACES ]] && [[ $CODESPACE_NAME ]]; then
+  CODESPACE_URL="${CODESPACE_NAME}-80.preview.app.github.dev"
+  URL_FRONT="https://${CODESPACE_URL}/"
+  URL_ADMIN="https://${CODESPACE_URL}/backend/"
+else
+  URL_FRONT="https://${TRAEFIK_DOMAIN}/"
+  URL_ADMIN="https://${TRAEFIK_DOMAIN}/backend/"
+fi
+
+## argument parsing
+## parse arguments
+while (( "$#" )); do
+    case "$1" in
+        --skip-db-import)
+            DB_IMPORT=
+            shift
+            ;;
+        --db-dump)
+            shift
+            DB_DUMP="$1"
+            shift
+            ;;
+        --no-pull)
+            AUTO_PULL=
+            shift
+            ;;
+        *)
+            error "Unrecognized argument '$1'"
+            exit -1
+            ;;
+    esac
+done
+
+## include check for DB_DUMP file only when database import is expected
+[[ ${DB_IMPORT} ]] && REQUIRED_FILES+=("${DB_DUMP}" "${WARDEN_WEB_ROOT}/app/etc/env.php.den.php")
+
+:: Verifying configuration
+INIT_ERROR=
+
+## attempt to install mutagen if not already present
+if [[ $OSTYPE =~ ^darwin ]] && ! which mutagen 2>/dev/null >/dev/null && which brew 2>/dev/null >/dev/null; then
+    warning "Mutagen could not be found; attempting install via brew."
+    brew install havoc-io/mutagen/mutagen
+fi
+
+## check for presence of host machine dependencies
+for DEP_NAME in den mutagen docker-compose pv; do
+  if [[ "${DEP_NAME}" = "mutagen" ]] && [[ ! $OSTYPE =~ ^darwin ]]; then
+    continue
+  fi
+
+  if ! which "${DEP_NAME}" 2>/dev/null >/dev/null; then
+    error "Command '${DEP_NAME}' not found. Please install."
+    INIT_ERROR=1
+  fi
+done
+
+## verify warden version constraint
+WARDEN_VERSION=$(den version 2>/dev/null) || true
+
+## copy global Marketplace credentials into webroot to satisfy REQUIRED_FILES list; in ideal
+## configuration the per-project auth.json will already exist with project specific keys
+if [[ ! -f "${WARDEN_WEB_ROOT}/auth.json" ]] && [[ -f ~/.composer/auth.json ]]; then
+  if docker run --rm -v ~/.composer/auth.json:/tmp/auth.json \
+      composer config -g http-basic.repo.magento.com >/dev/null 2>&1
+  then
+    warning "Configuring ${WARDEN_WEB_ROOT}/auth.json with global credentials for repo.magento.com"
+    echo "{\"http-basic\":{\"repo.magento.com\":$(
+      docker run --rm -v ~/.composer/auth.json:/tmp/auth.json composer config -g http-basic.repo.magento.com
+    )}}" > ${WARDEN_WEB_ROOT}/auth.json
+  fi
+fi
+
+## verify mutagen version constraint
+MUTAGEN_VERSION=$(mutagen version 2>/dev/null) || true
+MUTAGEN_REQUIRE=0.11.4
+if [[ $OSTYPE =~ ^darwin ]] && ! test $(version ${MUTAGEN_VERSION}) -ge $(version ${MUTAGEN_REQUIRE}); then
+  error "Mutagen ${MUTAGEN_REQUIRE} or greater is required (version ${MUTAGEN_VERSION} is installed)"
+  INIT_ERROR=1
+fi
+
+## check for presence of local configuration files to ensure they exist
+for REQUIRED_FILE in ${REQUIRED_FILES[@]}; do
+  if [[ ! -f "${REQUIRED_FILE}" ]]; then
+    error "Missing local file: ${REQUIRED_FILE}"
+    INIT_ERROR=1
+  fi
+done
+
+## exit script if there are any missing dependencies or configuration files
+[[ ${INIT_ERROR} ]] && exit 1
+
+:: Starting Warden
+# den svc up
+if [[ ! -f ~/.den/ssl/certs/${TRAEFIK_DOMAIN}.crt.pem ]]; then
+    den sign-certificate ${TRAEFIK_DOMAIN}
+fi
+
+:: Initializing environment
+if [[ $AUTO_PULL ]]; then
+  den env pull --ignore-pull-failures || true
+  den env build --pull
+else
+  den env build
+fi
+den env up -d
+
+## wait for mariadb to start listening for connections
+den shell -c "while ! nc -z db 3306 </dev/null; do sleep 2; done"
+
+:: Installing dependencies
+den env exec -T php-fpm composer install
+
+## import database only if --skip-db-import is not specified
+if [[ ${DB_IMPORT} ]]; then
+  :: Importing Magento database
+  den db connect -e 'drop database magento; create database magento;'
+  pv "${DB_DUMP}" | gunzip -c | den db import
+fi 
+
+den env exec -T php-fpm bin/magento app:config:import
+den env exec -T php-fpm bin/magento config:set -q web/unsecure/base_url ${URL_FRONT}
+den env exec -T php-fpm bin/magento config:set -q web/secure/base_url ${URL_FRONT}
+
+:: Flushing cache
+den env exec -T php-fpm bin/magento cache:flush
+den env exec -T php-fpm bin/magento cache:disable block_html full_page
+
+:: Creating admin user
+ADMIN_PASS=$(den env exec -T php-fpm pwgen -n1 16)
+ADMIN_USER=localadmin
+
+den env exec -T php-fpm bin/magento admin:user:create \
+    --admin-password="${ADMIN_PASS}" \
+    --admin-user="${ADMIN_USER}" \
+    --admin-firstname="Local" \
+    --admin-lastname="Admin" \
+    --admin-email="${ADMIN_USER}@example.com"
+
+:: Initialization complete
+function print_install_info {
+    FILL=$(printf "%0.s-" {1..128})
+    C1_LEN=8
+    let "C2_LEN=${#URL_ADMIN}>${#ADMIN_PASS}?${#URL_ADMIN}:${#ADMIN_PASS}"
+
+    # note: in CentOS bash .* isn't supported (is on Darwin), but *.* is more cross-platform
+    printf "+ %*.*s + %*.*s + \n" 0 $C1_LEN $FILL 0 $C2_LEN $FILL
+    printf "+ %-*s + %-*s + \n" $C1_LEN FrontURL $C2_LEN "$URL_FRONT"
+    printf "+ %*.*s + %*.*s + \n" 0 $C1_LEN $FILL 0 $C2_LEN $FILL
+    printf "+ %-*s + %-*s + \n" $C1_LEN AdminURL $C2_LEN "$URL_ADMIN"
+    printf "+ %*.*s + %*.*s + \n" 0 $C1_LEN $FILL 0 $C2_LEN $FILL
+    printf "+ %-*s + %-*s + \n" $C1_LEN Username $C2_LEN "$ADMIN_USER"
+    printf "+ %*.*s + %*.*s + \n" 0 $C1_LEN $FILL 0 $C2_LEN $FILL
+    printf "+ %-*s + %-*s + \n" $C1_LEN Password $C2_LEN "$ADMIN_PASS"
+    printf "+ %*.*s + %*.*s + \n" 0 $C1_LEN $FILL 0 $C2_LEN $FILL
+}
+print_install_info
